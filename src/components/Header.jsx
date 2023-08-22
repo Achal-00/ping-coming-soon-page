82b5c9ad310d7a566b1c4707f4253ff816747725
Achal <achaldev3@gmail.com>
2023-08-22 16:16:04 +0530

    version-1.2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+
 const Header = () => {
+  const [email, setEmail] = useState("");
+
+  var validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  function btnHandler() {
+    if (email === "" || !email.match(validRegex)) {
+      document.querySelector(".newsletter input").style.border =
+        "thin solid var(--light-red)";
+      document.querySelector(".newsletter p").style.display = "block";
+    } else {
+      document.querySelector(".newsletter input").style.border =
+        "thin solid green";
+      document.querySelector(".newsletter p").style.display = "block";
+      document.querySelector(".newsletter p").style.color = "green";
+      document.querySelector(".newsletter p").textContent =
+        "Thanks for subscribing!";
+    }
+  }
+
   return (
     <section className="header">
       <div className="logo-holder">
@@ -12,11 +34,15 @@ const Header = () => {
       </div>
       <div className="newsletter">
         <div className="input">
-          <input type="text" placeholder="Your email address..." />
+          <input
+            type="text"
+            placeholder="Your email address..."
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <p>Please provide a valid email address</p>
         </div>
         <div className="btn">
-          <button>Notify Me</button>
+          <button onClick={btnHandler}>Notify Me</button>
         </div>
       </div>
     </section>
